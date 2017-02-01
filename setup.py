533562afcc09273a714f23f46b4e272b08c36ef3
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-01 16:56:47 +0000

    finished setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,24 @@ cx_Freeze.setup(
     options={
         "build_exe": {
             "packages": ["pygame", "json", "os"],
-            "included_files": [
-
+            "excludes": ["tkinter"],
+            "include_files": [
+                "level_data/",
+                "resources/",
+                "constants.py",
+                "covers.py",
+                "entities.py",
+                "guards.py",
+                "healthbar.py",
+                "level.py",
+                "leveltext.py",
+                "platforms.py",
+                "player.py",
+                "spritesheet.py",
+                "terrain.py",
+                "torches.py"
             ]
         }
-    }
+    },
+    executables=executables
 )
