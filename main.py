68a6d826fccc4112e38dcabdf1ffe8f7d120e2a6
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-01 17:46:47 +0000

    Now runs in fullscreen

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ def main():
     pygame.init()
 
     # Set the display size
-    game_display = pygame.display.set_mode(constants.SIZE)
+    game_display = pygame.display.set_mode(constants.SIZE, flags=FULLSCREEN)
 
     # Set the window caption and icon
     pygame.display.set_caption("Stealth")
 
     icon_img = pygame.image.load("resources/icon.ico")
 
-    icon = pygame.Surface([32, 32], flags=pygame.SRCALPHA)
+    icon = pygame.Surface([32, 32], flags=SRCALPHA)
     icon = icon.convert_alpha()
     icon.blit(icon_img, (0, 0))
     pygame.display.set_icon(icon)
