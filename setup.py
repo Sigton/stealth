be004f79efa34f8d8fe70f52b81cd91384980c79
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-02 19:25:37 +0000

    Hopefully this works

--- setup.py
+++ setup.py
@@ -8,7 +8,21 @@ cx_Freeze.setup(
         "build_exe": {
             "packages": ["pygame", "json", "os"],
             "excludes": ["tkinter", "OpenGL"],
-            "include_files": ["source/"]
+            "include_files": ["source/level_data/",
+                              "source/resources/",
+                              "source/arms.py",
+                              "source/constants.py",
+                              "source/covers.py",
+                              "source/entities.py",
+                              "source/guards.py",
+                              "source/healthbar.py",
+                              "source/level.py",
+                              "source/leveltext.py",
+                              "source/platforms.py",
+                              "source/player.py",
+                              "source/spritesheet.py",
+                              "source/terrain.py",
+                              "source/torches.py"]
         }
     },
     executables=executables
