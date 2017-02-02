a07a2324d730f2525337ac7a9c6fbe923a5fe320
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-02 19:37:48 +0000

    Updated setup.py to include more excludes

--- setup.py
+++ setup.py
@@ -2,27 +2,40 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable("source/main.py")]
 
+include_files = ["source/level_data/",
+                 "source/resources/",
+                 "source/arms.py",
+                 "source/constants.py",
+                 "source/covers.py",
+                 "source/entities.py",
+                 "source/guards.py",
+                 "source/healthbar.py",
+                 "source/level.py",
+                 "source/leveltext.py",
+                 "source/platforms.py",
+                 "source/player.py",
+                 "source/spritesheet.py",
+                 "source/terrain.py",
+                 "source/torches.py"]
+
+excludes = ["tkinter",
+            "OpenGL",
+            "email",
+            "html",
+            "http",
+            "multiprocessing",
+            "numpy",
+            "urllib",
+            "xml",
+            "socket"]
+
 cx_Freeze.setup(
     name="Stealth",
     options={
         "build_exe": {
             "packages": ["pygame", "json", "os"],
-            "excludes": ["tkinter", "OpenGL"],
-            "include_files": ["source/level_data/",
-                              "source/resources/",
-                              "source/arms.py",
-                              "source/constants.py",
-                              "source/covers.py",
-                              "source/entities.py",
-                              "source/guards.py",
-                              "source/healthbar.py",
-                              "source/level.py",
-                              "source/leveltext.py",
-                              "source/platforms.py",
-                              "source/player.py",
-                              "source/spritesheet.py",
-                              "source/terrain.py",
-                              "source/torches.py"]
+            "excludes": excludes,
+            "include_files": include_files
         }
     },
     executables=executables
