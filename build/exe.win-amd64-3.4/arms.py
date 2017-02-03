698e1631a39c3c3c83a7d18e16da82593d7dd764
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-03 08:25:56 +0000

    Created new build

--- build/exe.win-amd64-3.4/arms.py
+++ build/exe.win-amd64-3.4/arms.py
@@ -30,10 +30,32 @@ class Arm(pygame.sprite.Sprite):
         if self.guard.direction == "R":
             self.rect.x = self.guard.rect.x + self.guard.rect.width / 4
         else:
-            self.rect.x = self.guard.rect.x + self.guard.rect.width * 0.6
+            self.rect.x = self.guard.rect.x + self.guard.rect.width * -0.4
         self.rect.y = self.guard.rect.y + self.guard.rect.height / 4
 
         if self.guard.direction == "R":
             self.image = self.arm_right
         else:
             self.image = self.arm_left
+
+        # Calculate the angle at which to point at
+
+        dx = self.guard.rect.x - self.guard.player.rect.x
+        dy = self.guard.rect.y - self.guard.player.rect.y
+
+        # Gotta love trig
+        rads = math.atan2(-dy, dx)
+        rads %= 2 * math.pi
+        degrees = math.degrees(rads)
+        degrees = (degrees + 180) % 360
+
+        # Rotate the image and reset the rect
+        self.image = pygame.transform.rotate(self.image, degrees)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+        # Align rectangle after rotation
+        self.rect.bottom = self.guard.rect.y + (self.guard.rect.height * 0.5)
+        if self.guard.direction == "R":
+            self.rect.left = self.guard.rect.left + self.guard.rect.width * 0.2
+        else:
+            self.rect.right = self.guard.rect.right - self.guard.rect.width * 0.2
