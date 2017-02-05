a7031e93fee33a17aae218a2564fb2fc4dcf34c0
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-05 19:04:58 +0000

    Finished leveltext class

--- source/leveltext.py
+++ source/leveltext.py
@@ -9,3 +9,11 @@ class LevelText(pygame.sprite.Sprite):
     def __init__(self, text, x, y):
 
         pygame.sprite.Sprite.__init__(self)
+
+        self.font = pygame.font.Font("resources/alienleague.ttf", 18)
+
+        self.image = self.font.render(text, True, constants.WHITE)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
