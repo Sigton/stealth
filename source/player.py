71c72b7af2491c10967c5ce3a99f4242bc567753
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-09 08:15:37 +0000

    Player walks slower on ladders

--- source/player.py
+++ source/player.py
@@ -211,13 +211,13 @@ class Player(pygame.sprite.Sprite):
     def walk_right(self):
 
         # Moves the player right
-        self.xv += self.speed / 1.5 if self.crouching else self.speed
+        self.xv += self.speed / 1.5 if self.crouching or self.climbing else self.speed
         self.direction = "R"
 
     def walk_left(self):
 
         # Moves the player left
-        self.xv -= self.speed / 2 if self.crouching else self.speed
+        self.xv -= self.speed / 2 if self.crouching or self.climbing else self.speed
         self.direction = "L"
 
     def jump(self):
