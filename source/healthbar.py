5fd43b461446935cf6acfad51d6ff53e6c9d87fc
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-13 12:52:35 +0000

    Updated health bar

--- source/healthbar.py
+++ source/healthbar.py
@@ -15,37 +15,37 @@ class HealthBar(pygame.sprite.Sprite):
 
         self.images = []
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 0, 32, 16)
+        image = self.sprite_sheet.get_image(0, 0, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 16, 32, 16)
+        image = self.sprite_sheet.get_image(0, 16, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 32, 32, 16)
+        image = self.sprite_sheet.get_image(0, 32, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 48, 32, 16)
+        image = self.sprite_sheet.get_image(0, 48, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 64, 32, 16)
+        image = self.sprite_sheet.get_image(0, 64, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 80, 32, 16)
+        image = self.sprite_sheet.get_image(0, 80, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 0, 32, 16)
+        image = self.sprite_sheet.get_image(32, 0, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 16, 32, 16)
+        image = self.sprite_sheet.get_image(32, 16, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 32, 32, 16)
+        image = self.sprite_sheet.get_image(32, 32, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 48, 32, 16)
+        image = self.sprite_sheet.get_imagea(32, 48, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 64, 32, 16)
+        image = self.sprite_sheet.get_image(32, 64, 32, 16)
         self.images.append(image)
 
         self.image = self.images[10]
@@ -72,37 +72,37 @@ class ProgressBar(pygame.sprite.Sprite):
 
         self.images = []
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 0, 32, 16)
+        image = self.sprite_sheet.get_image(0, 0, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 16, 32, 16)
+        image = self.sprite_sheet.get_image(0, 16, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 32, 32, 16)
+        image = self.sprite_sheet.get_image(0, 32, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 48, 32, 16)
+        image = self.sprite_sheet.get_image(0, 48, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 64, 32, 16)
+        image = self.sprite_sheet.get_image(0, 64, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(0, 80, 32, 16)
+        image = self.sprite_sheet.get_image(0, 80, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 0, 32, 16)
+        image = self.sprite_sheet.get_image(32, 0, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 16, 32, 16)
+        image = self.sprite_sheet.get_image(32, 16, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 32, 32, 16)
+        image = self.sprite_sheet.get_image(32, 32, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 48, 32, 16)
+        image = self.sprite_sheet.get_image(32, 48, 32, 16)
         self.images.append(image)
 
-        image = self.sprite_sheet.get_image_srcalpha(32, 64, 32, 16)
+        image = self.sprite_sheet.get_image(32, 64, 32, 16)
         self.images.append(image)
 
         self.image = self.images[0]
