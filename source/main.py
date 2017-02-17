b0186dced284f627361a8ca45ba0056e5e172d23
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-17 22:35:03 +0000

    Removed unused files

--- source/main.py
+++ source/main.py
@@ -59,8 +59,8 @@ def main():
     # Create the levels
     level_list = list()
 
-    #level_list.append(level.Level01(player, True))
-    level_list.append(level.Level02(player, True))
+    # level_list.append(level.Level01(player, True))
+    # level_list.append(level.Level02(player, True))
     level_list.append(level.Level03(player, True))
     level_list.append(level.Level04(player, True))
 
