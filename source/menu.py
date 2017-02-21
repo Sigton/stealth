4ee29c430164393f02d1a28cef5dc898277c6f0f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-21 21:13:54 +0000

    Updated menu

--- source/menu.py
+++ source/menu.py
@@ -12,10 +12,17 @@ class Button(pygame.sprite.Sprite):
 
         self.sprite_sheet = spritesheet.SpriteSheet(sprite_sheet)
 
-        self.image = self.sprite_sheet.get_image(sprite_sheet_data[0],
-                                                 sprite_sheet_data[1],
-                                                 sprite_sheet_data[2],
-                                                 sprite_sheet_data[3])
+        self.image_inactive = self.sprite_sheet.get_image(sprite_sheet_data[0][0],
+                                                          sprite_sheet_data[0][1],
+                                                          sprite_sheet_data[0][2],
+                                                          sprite_sheet_data[0][3])
+
+        self.image_active = self.sprite_sheet.get_image(sprite_sheet_data[1][0],
+                                                        sprite_sheet_data[1][1],
+                                                        sprite_sheet_data[1][2],
+                                                        sprite_sheet_data[1][3])
+
+        self.image = self.image_inactive
 
         self.rect = self.image.get_rect()
 
@@ -28,6 +35,11 @@ class Menu:
         self.clock = clock
 
         self.game = g.Game(display, clock)
+
+        main_menu_buttons = list()
+        main_menu_buttons.append(Button("resources/menubuttons.png", ((0, 80, 360, 80), (360, 0, 360, 80))))
+        main_menu_buttons.append(Button("resources/menubuttons.png", ((0, 80, 360, 80), (360, 0, 360, 80))))
+        main_menu_buttons.append(Button("resources/menubuttons.png", ((0, 80, 360, 80), (360, 0, 360, 80))))
 
     def run(self):
 
