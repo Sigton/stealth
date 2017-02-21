eef48ceaab81876a9fccb0988a1193eb0c566d5d
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-21 20:44:52 +0000

    Updated menu.py

--- source/menu.py
+++ source/menu.py
@@ -1,6 +1,11 @@
+import game as g
+
+
 class Menu:
 
     def __init__(self, display, clock):
 
         self.display = display
         self.clock = clock
+
+        self.game = g.Game(display, clock)
