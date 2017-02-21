4b531aaf6efa89234bca5ce479285c6c73259264
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-21 20:32:42 +0000

    Game is now a class

--- source/game.py
+++ source/game.py
@@ -12,223 +12,230 @@ from spritesheet import blit_alpha
 from funcs import pixel_perfect_collision
 
 
-def game(game_display, clock):
-    # Game loop
+class Game:
 
-    # Show the loading screen
-    loading_screen = covers.LoadingScreen()
+    def __init__(self, display, clock):
 
-    for n in range(63):
-        blit_alpha(game_display, loading_screen.image, (0, 0), n*4)
-        pygame.display.flip()
+        self.display = display
+        self.clock = clock
 
-    # Load the music
-    pygame.mixer.music.load("resources/music.mp3")
-    pygame.mixer.music.set_volume(0.25)
+    def game(self):
+        # Game loop
 
-    # Create the player
-    player = p.Player()
+        # Show the loading screen
+        loading_screen = covers.LoadingScreen()
 
-    active_sprite_list = pygame.sprite.Group()
-    active_sprite_list.add(player)
+        for n in range(63):
+            blit_alpha(self.display, loading_screen.image, (0, 0), n*4)
+            pygame.display.flip()
 
-    # Create the levels
-    level_list = list()
+        # Load the music
+        pygame.mixer.music.load("resources/music.mp3")
+        pygame.mixer.music.set_volume(0.25)
 
-    level_list.append(level.Level01(player, True))
-    level_list.append(level.Level02(player, True))
-    level_list.append(level.Level03(player, True))
-    level_list.append(level.Level04(player, True))
+        # Create the player
+        player = p.Player()
 
-    # Set the current level
-    current_level_no = 0
-    current_level = level_list[current_level_no]
+        active_sprite_list = pygame.sprite.Group()
+        active_sprite_list.add(player)
 
-    player.level = current_level
-    current_level.player = player
+        # Create the levels
+        level_list = list()
 
-    # Create the blackout
-    blackout = covers.Blackout()
-    blackout.player = player
+        level_list.append(level.Level01(player, True))
+        level_list.append(level.Level02(player, True))
+        level_list.append(level.Level03(player, True))
+        level_list.append(level.Level04(player, True))
 
-    crosshair = entities.Crosshair()
+        # Set the current level
+        current_level_no = 0
+        current_level = level_list[current_level_no]
 
-    # Set the players position
-    player.rect.x = 48
-    player.rect.y = 384
+        player.level = current_level
+        current_level.player = player
 
-    # Variables to control the player
-    run = 0
-    jump = False
-    crouch = False
+        # Create the blackout
+        blackout = covers.Blackout()
+        blackout.player = player
 
-    pause = 0
-    reset = False
+        crosshair = entities.Crosshair()
 
-    light_sound = pygame.mixer.Sound("resources/lights.wav")
-    light_sound.set_volume(0.15)
+        # Set the players position
+        player.rect.x = 48
+        player.rect.y = 384
 
-    has_guard = False
-    for guard in current_level.guards.sprites():
-        if isinstance(guard, guards.Guard):
-            has_guard = True
-    if has_guard:
-        light_sound.play(-1)
+        # Variables to control the player
+        run = 0
+        jump = False
+        crouch = False
 
-    # Play the music
-    pygame.mixer.music.play(-1)
+        pause = 0
+        reset = False
 
-    # Loop until the window is closed
-    game_exit = False
+        light_sound = pygame.mixer.Sound("resources/lights.wav")
+        light_sound.set_volume(0.15)
 
-    while not game_exit:
-        for event in pygame.event.get():
+        has_guard = False
+        for guard in current_level.guards.sprites():
+            if isinstance(guard, guards.Guard):
+                has_guard = True
+        if has_guard:
+            light_sound.play(-1)
 
-            # If player closes window
-            if event.type == QUIT:
-                game_exit = True
+        # Play the music
+        pygame.mixer.music.play(-1)
 
-            elif event.type == KEYDOWN:
+        # Loop until the window is closed
+        game_exit = False
 
-                if event.key == K_ESCAPE:
+        while not game_exit:
+            for event in pygame.event.get():
+
+                # If player closes window
+                if event.type == QUIT:
                     game_exit = True
 
-                # Player controls
-                if event.key == K_LEFT or event.key == K_a:
-                    run = -1
-                if event.key == K_RIGHT or event.key == K_d:
-                    run = 1
+                elif event.type == KEYDOWN:
 
-                if event.key == K_UP or event.key == K_w:
-                    jump = True
+                    if event.key == K_ESCAPE:
+                        game_exit = True
 
-                # Use keypads
-                if event.key == K_SPACE:
-                    player.use_keypad()
+                    # Player controls
+                    if event.key == K_LEFT or event.key == K_a:
+                        run = -1
+                    if event.key == K_RIGHT or event.key == K_d:
+                        run = 1
 
-                # Crouching
-                if event.key == K_LCTRL:
-                    crouch = True
+                    if event.key == K_UP or event.key == K_w:
+                        jump = True
 
-            elif event.type == KEYUP:
+                    # Use keypads
+                    if event.key == K_SPACE:
+                        player.use_keypad()
 
-                if event.key == K_LEFT or event.key == K_RIGHT:
-                    run = 0
+                    # Crouching
+                    if event.key == K_LCTRL:
+                        crouch = True
 
-                if event.key == K_a or event.key == K_d:
-                    run = 0
+                elif event.type == KEYUP:
 
-                if event.key == K_UP or event.key == K_w:
-                    jump = False
-                    player.climbing = False
+                    if event.key == K_LEFT or event.key == K_RIGHT:
+                        run = 0
 
-                if event.key == K_LCTRL:
-                    crouch = False
+                    if event.key == K_a or event.key == K_d:
+                        run = 0
 
-        if pause > 0:
-            pause -= 1
+                    if event.key == K_UP or event.key == K_w:
+                        jump = False
+                        player.climbing = False
 
-        if pause == 0 and reset:
-            current_level.reset_world()
-            current_level.set_scrolling()
-            player.reset()
-            reset = False
+                    if event.key == K_LCTRL:
+                        crouch = False
 
-        # Level progression
-        if player.rect.x + player.rect.width/2 >= constants.SCREEN_WIDTH:
+            if pause > 0:
+                pause -= 1
 
-            # Reset the player and move on the level
-            current_level.reset_world()
-            player.reset()
+            if pause == 0 and reset:
+                current_level.reset_world()
+                current_level.set_scrolling()
+                player.reset()
+                reset = False
 
-            light_sound.stop()
+            # Level progression
+            if player.rect.x + player.rect.width/2 >= constants.SCREEN_WIDTH:
 
-            current_level_no += 1
-            if current_level_no >= len(level_list):
-                break
-            else:
-                current_level = level_list[current_level_no]
+                # Reset the player and move on the level
+                current_level.reset_world()
+                player.reset()
 
-            player.level = current_level
-            current_level.player = player
+                light_sound.stop()
 
-            has_guard = False
-            for guard in current_level.guards.sprites():
-                if isinstance(guard, guards.Guard):
-                    has_guard = True
-            if has_guard:
-                light_sound.play(-1)
+                current_level_no += 1
+                if current_level_no >= len(level_list):
+                    break
+                else:
+                    current_level = level_list[current_level_no]
 
-        # Check if player has hit obstacles
-        obstacle_hits = pygame.sprite.spritecollide(player, current_level.obstacle_list, False)
-        if len(obstacle_hits):
-            player.reset()
-            current_level.reset_world()
-            current_level.set_scrolling()
+                player.level = current_level
+                current_level.player = player
 
-        if not pause:
-            # Check if the guards got the players
-            hit_list = pygame.sprite.spritecollide(player, current_level.entities, False)
-            for hit in hit_list:
-                if isinstance(hit, torches.Torch):
-                    if pixel_perfect_collision(player, hit):
-                        pause = 120
-                        reset = True
+                has_guard = False
+                for guard in current_level.guards.sprites():
+                    if isinstance(guard, guards.Guard):
+                        has_guard = True
+                if has_guard:
+                    light_sound.play(-1)
 
-            # Playing running and jumping
-            if abs(run) > 0:
-                if run == 1:
-                    player.walk_right()
-                elif run == -1:
-                    player.walk_left()
+            # Check if player has hit obstacles
+            obstacle_hits = pygame.sprite.spritecollide(player, current_level.obstacle_list, False)
+            if len(obstacle_hits):
+                player.reset()
+                current_level.reset_world()
+                current_level.set_scrolling()
 
-            if crouch:
-                player.do_crouch()
-            else:
-                player.stop_crouching()
+            if not pause:
+                # Check if the guards got the players
+                hit_list = pygame.sprite.spritecollide(player, current_level.entities, False)
+                for hit in hit_list:
+                    if isinstance(hit, torches.Torch):
+                        if pixel_perfect_collision(player, hit):
+                            pause = 120
+                            reset = True
 
-            if jump and not crouch:
-                player.jump()
+                # Playing running and jumping
+                if abs(run) > 0:
+                    if run == 1:
+                        player.walk_right()
+                    elif run == -1:
+                        player.walk_left()
 
-            # Update entities
-        active_sprite_list.update()
-        if not pause:
-            current_level.update()
-        blackout.update()
-        crosshair.update()
+                if crouch:
+                    player.do_crouch()
+                else:
+                    player.stop_crouching()
 
-        # Scrolling
-        if player.rect.x >= 624:
-            diff = player.rect.x - 624
-            if not current_level.at_edge_x:
-                player.rect.x = 624
-            current_level.shift_world(-diff, 0)
+                if jump and not crouch:
+                    player.jump()
 
-        if player.rect.x <= 288:
-            diff = player.rect.x - 288
-            if not current_level.at_edge_x:
-                player.rect.x = 288
-            current_level.shift_world(-diff, 0)
+                # Update entities
+            active_sprite_list.update()
+            if not pause:
+                current_level.update()
+            blackout.update()
+            crosshair.update()
 
-        if player.rect.y >= 454:
-            diff = player.rect.y - 454
-            if not current_level.at_edge_y:
-                player.rect.y = 454
-            current_level.shift_world(0, diff)
+            # Scrolling
+            if player.rect.x >= 624:
+                diff = player.rect.x - 624
+                if not current_level.at_edge_x:
+                    player.rect.x = 624
+                current_level.shift_world(-diff, 0)
 
-        if player.rect.y <= 288:
-            diff = player.rect.y - 288
-            if not current_level.at_edge_y:
-                player.rect.y = 288
-            current_level.shift_world(0, diff)
+            if player.rect.x <= 288:
+                diff = player.rect.x - 288
+                if not current_level.at_edge_x:
+                    player.rect.x = 288
+                current_level.shift_world(-diff, 0)
 
-        # All drawing goes here
-        current_level.draw(game_display)
-        active_sprite_list.draw(game_display)
-        blackout.draw(game_display)
-        crosshair.draw(game_display)
+            if player.rect.y >= 454:
+                diff = player.rect.y - 454
+                if not current_level.at_edge_y:
+                    player.rect.y = 454
+                current_level.shift_world(0, diff)
 
-        # Limit to 60 fps
-        clock.tick(60)
-        # Update the display
-        pygame.display.flip()
+            if player.rect.y <= 288:
+                diff = player.rect.y - 288
+                if not current_level.at_edge_y:
+                    player.rect.y = 288
+                current_level.shift_world(0, diff)
+
+            # All drawing goes here
+            current_level.draw(self.display)
+            active_sprite_list.draw(self.display)
+            blackout.draw(self.display)
+            crosshair.draw(self.display)
+
+            # Limit to 60 fps
+            self.clock.tick(60)
+            # Update the display
+            pygame.display.flip()
