12e88b51ddc8672c758875eb296f3cde1016dacd
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-21 21:03:23 +0000

    Added button class

--- source/menu.py
+++ source/menu.py
@@ -1,4 +1,23 @@
+import pygame
+
 import game as g
+import spritesheet
+
+
+class Button(pygame.sprite.Sprite):
+
+    def __init__(self, sprite_sheet, sprite_sheet_data):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.sprite_sheet = spritesheet.SpriteSheet(sprite_sheet)
+
+        self.image = self.sprite_sheet.get_image(sprite_sheet_data[0],
+                                                 sprite_sheet_data[1],
+                                                 sprite_sheet_data[2],
+                                                 sprite_sheet_data[3])
+
+        self.rect = self.image.get_rect()
 
 
 class Menu:
