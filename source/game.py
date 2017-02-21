14b8133237eaeba03d47a519546af7cf0bef1e7b
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-21 20:02:03 +0000

    Updated things

--- source/game.py
+++ source/game.py
@@ -12,24 +12,8 @@ from spritesheet import blit_alpha
 from funcs import pixel_perfect_collision
 
 
-def game():
+def game(game_display):
     # Game loop
-
-    # Set the display size
-    game_display = pygame.display.set_mode(constants.SIZE)
-
-    # Hide the mouse
-    pygame.mouse.set_visible(False)
-
-    # Set the window caption and icon
-    pygame.display.set_caption("Stealth")
-
-    icon_img = pygame.image.load("resources/icon.ico")
-
-    icon = pygame.Surface([32, 32], flags=SRCALPHA)
-    icon = icon.convert_alpha()
-    icon.blit(icon_img, (0, 0))
-    pygame.display.set_icon(icon)
 
     # Show the loading screen
     loading_screen = covers.LoadingScreen()
@@ -251,7 +235,3 @@ def game():
         clock.tick(60)
         # Update the display
         pygame.display.flip()
-
-    pygame.mouse.set_visible(True)
-    pygame.quit()
-    quit()
--- source/main.py
+++ source/main.py
@@ -1,6 +1,8 @@
 import pygame
+from pygame.locals import *
 
 import game
+import constants
 
 
 def main():
@@ -12,7 +14,27 @@ def main():
     pygame.mixer.init()
     pygame.init()
 
-    game.game()
+    # Set the display size
+    game_display = pygame.display.set_mode(constants.SIZE)
+
+    # Hide the mouse
+    pygame.mouse.set_visible(False)
+
+    # Set the window caption and icon
+    pygame.display.set_caption("Stealth")
+
+    icon_img = pygame.image.load("resources/icon.ico")
+
+    icon = pygame.Surface([32, 32], flags=SRCALPHA)
+    icon = icon.convert_alpha()
+    icon.blit(icon_img, (0, 0))
+    pygame.display.set_icon(icon)
+
+    game.game(game_display)
+
+    pygame.mouse.set_visible(True)
+    pygame.quit()
+    quit()
 
 if __name__ == "__main__":
     main()
