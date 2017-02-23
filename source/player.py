9f351ee9d0340f9eac660659f1448f87ea17d98c
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-02-23 19:57:06 +0000

    Fixed bug

--- source/player.py
+++ source/player.py
@@ -122,11 +122,10 @@ class Player(pygame.sprite.Sprite):
         elif self.touching_ladder and not self.climbing:
             self.yv = self.climb_speed
         else:
-            if not (self.crouching and not self.climbing):
-                self.yv += self.gravity
+            self.yv += self.gravity
 
-                if self.yv >= 15:
-                    self.yv = 15
+            if self.yv >= 15:
+                self.yv = 15
 
         if self.rect.y >= constants.SCREEN_HEIGHT - self.rect.height and self.yv >= 0:
             self.yv = 0
