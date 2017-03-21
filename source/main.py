71ba47663842a7a60fc63357782ba1c9e8a13ba0
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-03-21 21:49:43 +0000

    Updated Launcher GUI layout

--- source/main.py
+++ source/main.py
@@ -47,24 +47,24 @@ class Launcher(tk.Frame):
         self.game = None
 
         self.title = ttk.Label(self, text="Stealth", font=("Verdana", 16))
-        self.title.place(x=240, y=40, anchor="center")
+        self.title.place(x=240, y=20, anchor="center")
 
         self.desc = ttk.Label(self, text="Select the launch options you want for your game.")
-        self.desc.place(x=240, y=80, anchor="center")
+        self.desc.place(x=240, y=60, anchor="center")
 
         self.button1 = ttk.Checkbutton(self, text="Fast Mode", variable=self.fast)
-        self.button1.place(x=50, y=120, anchor="w")
+        self.button1.place(x=50, y=100, anchor="w")
         self.label1 = ttk.Label(self, text="Removes decorations for better performance.",
                                 font="Helvetica 9 italic")
-        self.label1.place(x=160, y=120, anchor="w")
+        self.label1.place(x=160, y=100, anchor="w")
 
         self.button2 = ttk.Checkbutton(self, text="Small Screen", variable=self.small)
-        self.button2.place(x=50, y=150, anchor="w")
+        self.button2.place(x=50, y=130, anchor="w")
         self.label2 = ttk.Label(self, text="Smaller window for smaller or lower resolution monitors.",
                                 font="Helvetica 9 italic")
-        self.label2.place(x=160, y=150, anchor="w")
+        self.label2.place(x=160, y=130, anchor="w")
 
-        self.launch_button = tk.Button(self, text="Launch", width="20", height="2", command=self.launch)
+        self.launch_button = tk.Button(self, text="Launch", width="20", height="2", bg="#aaa", command=self.launch)
         self.launch_button.place(x=240, y=320, anchor="center")
 
     def launch(self):
