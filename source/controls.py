9b6c7d8f2dfec1f60ae6fac599893d8ed61007ef
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-03-21 18:59:09 +0000

    Created function for getting last key pressed

--- source/controls.py
+++ source/controls.py
@@ -1,3 +1,4 @@
+import pygame
 from pygame.locals import *
 
 # The control for each function
@@ -7,3 +8,18 @@ WALK_RIGHT = K_d
 JUMP = K_w
 ACTION = K_SPACE
 CROUCH = K_LCTRL
+
+
+def get_last_key():
+
+    key_pressed = False
+    while not key_pressed:
+
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+
+                if event.key == K_ESCAPE:
+                    return None
+
+                else:
+                    return event.key
