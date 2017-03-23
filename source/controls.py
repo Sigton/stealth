97cbaaa1af415b48a16c88890a80bb9f06141ec7
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-03-23 08:01:04 +0000

    That function wasn't working, time to find an alternate method

--- source/controls.py
+++ source/controls.py
@@ -1,4 +1,3 @@
-import pygame
 from pygame.locals import *
 
 # The control for each function
@@ -8,19 +7,3 @@ WALK_RIGHT = K_d
 JUMP = K_w
 ACTION = K_SPACE
 CROUCH = K_LCTRL
-
-
-def get_last_key():
-
-    key = None
-
-    key_pressed = False
-    while not key_pressed:
-        pygame.event.pump()
-        keys = pygame.key.get_pressed()
-        print(keys)
-        if 1 in keys:
-            key = keys.index(1)
-            key_pressed = True
-
-    return key
