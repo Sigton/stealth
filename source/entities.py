4b12c0d3936e2f47034a1cc622c9675e9ea50f3a
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-04-15 22:56:47 +0100

    And the sky works on the y-axis too

--- source/entities.py
+++ source/entities.py
@@ -401,5 +401,7 @@ class Sky(pygame.sprite.Sprite):
         # This is complicated for the sky
         # since it's position needs to move relative to the rest of the tiles
         self.image.fill(constants.BLACK)
-        self.image.blit(self.full_image, ((self.level.world_shift_x//-4)-240, 0))
+
+        self.image.blit(self.full_image, ((self.level.world_shift_x//-4)-240,
+                                          (self.level.world_shift_y//4)))
         display.blit(self.image, (self.rect.x, self.rect.y))
