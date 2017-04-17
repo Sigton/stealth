f6bfd0f2ad539a992b34505aa08a032a887036d7
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-04-17 17:46:53 +0100

    Updated setup.py to compile source files before building exe

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
 import cx_Freeze
+import py_compile
+import os
+
+# First of compile of the source files, since they
+# take up less storage space when compiled
+
+source_files = ["source/guard_parts.py",
+                "source/constants.py",
+                "source/covers.py",
+                "source/entities.py",
+                "source/guards.py",
+                "source/progressbar.py",
+                "source/level.py",
+                "source/text.py",
+                "source/platforms.py",
+                "source/player.py",
+                "source/spritesheet.py",
+                "source/terrain.py",
+                "source/torches.py",
+                "source/funcs.py",
+                "source/game.py",
+                "source/menu.py",
+                "source/hud.py",
+                "source/saves.py",
+                "source/sounds.py"]
+
+for file in source_files:
+    py_compile.compile(file, os.path.join("compiled", file[7:]+"c"))
 
 executables = [cx_Freeze.Executable(script="source/main.py",
                                     icon="source/resources/icon.ico",
@@ -6,25 +34,25 @@ executables = [cx_Freeze.Executable(script="source/main.py",
 
 include_files = ["source/level_data/",
                  "source/resources/",
-                 "source/guard_parts.py",
-                 "source/constants.py",
-                 "source/covers.py",
-                 "source/entities.py",
-                 "source/guards.py",
-                 "source/progressbar.py",
-                 "source/level.py",
-                 "source/text.py",
-                 "source/platforms.py",
-                 "source/player.py",
-                 "source/spritesheet.py",
-                 "source/terrain.py",
-                 "source/torches.py",
-                 "source/funcs.py",
-                 "source/game.py",
-                 "source/menu.py",
-                 "source/hud.py",
-                 "source/saves.py",
-                 "source/sounds.py",
+                 "compiled/guard_parts.pyc",
+                 "compiled/constants.pyc",
+                 "compiled/covers.pyc",
+                 "compiled/entities.pyc",
+                 "compiled/guards.pyc",
+                 "compiled/progressbar.pyc",
+                 "compiled/level.pyc",
+                 "compiled/text.pyc",
+                 "compiled/platforms.pyc",
+                 "compiled/player.pyc",
+                 "compiled/spritesheet.pyc",
+                 "compiled/terrain.pyc",
+                 "compiled/torches.pyc",
+                 "compiled/funcs.pyc",
+                 "compiled/game.pyc",
+                 "compiled/menu.pyc",
+                 "compiled/hud.pyc",
+                 "compiled/saves.pyc",
+                 "compiled/sounds.pyc",
                  "source/save_data.json"]
 
 excludes = ["OpenGL",
